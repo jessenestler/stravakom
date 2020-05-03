fa71504460693aaa71fa3f65aca42ae30b33c118
Jesse Nestler <jesse.nestler@colorado.edu>
2020-05-02 18:29:13 -0600

    Extract activities from strava API and parse

---
+++ get_activities.sh
@@ -0,0 +1,25 @@
+#! /bin/sh
+my_token=$(. refresh_auth.sh)
+
+# Get activity information
+if [[ ! -f activities.json ]]
+then
+    curl -X GET https://www.strava.com/api/v3/athlete/activities \
+    -d access_token=$my_token >\
+    activities.json
+fi
+
+# Parse each activity into a new file by ID
+c=$(jq '. | length' activities.json)
+for x in $(seq 0 $(($c-1)))
+do
+    act_id=$(jq -r " .[$x] | .id" activities.json)
+    if [[ ! -f ./data/$act_id'_activity.json' ]]
+    then
+        echo "Extracting metadata for Activity $act_id..."
+        jq ".[$x]" activities.json >\
+        ./data/$act_id'_activity.json'
+    else
+        echo "Activity information already exists for $act_id"
+    fi
+done
