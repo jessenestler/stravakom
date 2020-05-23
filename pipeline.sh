832a23a431a13c5c1206925a33ec8eec13e48768
Jesse Nestler <jesse.nestler@colorado.edu>
2020-05-23 17:42:54 -0600

    Run python script in pipeline

--- pipeline.sh
+++ pipeline.sh
@@ -1,3 +1,8 @@
+# run the strava api scraper via python and conda
+conda activate stravakom_env
+python strava_api.py
+conda deactivate
+
 # add contents of each file to the sandbox.ingested_data table
 if [[ "$(ls ./data/*.json)" ]]; then
     for file in ./data/*.json
