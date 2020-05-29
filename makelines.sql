431e6428dffb23a3bcc5d7c47a0a1c1c8c881dbf
Jesse Nestler <jesse.nestler@colorado.edu>
2020-05-28 20:50:13 -0600

    Create spatial index on raw_lines table

--- makelines.sql
+++ makelines.sql
@@ -1,10 +1,14 @@
--- Make line fatures out of the GPS streams
-select 	    act.*,
-		    ST_MakeLine(
-			  gps.shape::geometry
-			  order by gps.seconds_from_start
-		    ) as geom
-    from	sandbox.streams gps
-    join	sandbox.activity act using(activity_id)
-group by	act.activity_id
-order by	act.activity_start desc;
+-- Make raw line fatures out of the GPS streams
+drop table if exists sandbox.raw_lines;
+create table sandbox.raw_lines as (
+	select 	    act.*,
+				ST_MakeLine(
+				  gps.shape::geometry
+				  order by gps.seconds_from_start
+				) as geom
+		from	sandbox.streams gps
+		join	sandbox.activity act using(activity_id)
+	group by	act.activity_id
+);
+
+create index raw_lines_idx on sandbox.raw_lines using gist(geom);
